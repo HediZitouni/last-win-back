ebec93850bf31fca1ba6f07413cad658491f509e
HediZitouni <zitouni.hedi1@gmail.com>
2022-08-26 12:51:07 +0200

    change port to 3000

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import cors from "cors";
 import { getLast, getOrCreateLast, updateLast } from "./last/last.lib";
 import { logCalls } from "./middlewares/log-calls.middleware";
 const app = express();
-const port = 443;
+const port = 3000;
 
 app.use(cors());
 app.use(express.json());
